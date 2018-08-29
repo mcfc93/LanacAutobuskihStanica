72d110827b806d878b7dcda0ab0db5e1411c4ec3
markoknezic <mknezic88@gmail.com>
2018-08-29 21:53:48 +0200

    Komunikacija sa bazom, dodan konektor za bazu i kod za logovanje,treba doraditi

--- src/org/unibl/etf/prijava/PrijavaController.java
+++ src/org/unibl/etf/prijava/PrijavaController.java
@@ -2,6 +2,16 @@ package org.unibl.etf.prijava;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javax.swing.JOptionPane;
+
+//import za BAZU
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 //import java.util.logging.Level;
 
 import javafx.event.ActionEvent;
@@ -37,8 +47,59 @@ public class PrijavaController implements Initializable {
 	
 	@FXML
     void prijava(ActionEvent event) {
-		//salterski radnik
-		try {
+		   	String ispis;
+			Connection c = null;
+	        Statement s = null;
+	        ResultSet r = null; 
+	        try{
+	            // 1.Get a connection to database
+	            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/bus","root","student");
+	            // 2.Create a statement
+	            s = c.createStatement();
+	            // 3.Execute a sql query
+	            r = s.executeQuery("select Ime, lozinka from zaposleni");
+	            // 4.Process the result set 
+	            while(r.next()){
+	                if(r.getString("Ime").equals(korisnickoImeTextField.getText()) && r.getString("lozinka").equals(lozinkaTextField.getText())){
+	                	try {
+	            			//																//org.unibl.etf.prijava.
+	            			Parent root = (AnchorPane)FXMLLoader.load(getClass().getResource("SalterskiRadnikView.fxml"));
+	            			Scene scene = new Scene(root);
+	            			Stage stage=new Stage();
+	            			stage.setScene(scene);
+	            			stage.setTitle("Šalterski radnik");
+	            			stage.setResizable(false);
+	            			//primaryStage.initStyle(StageStyle.DECORATED);
+	            			//primaryStage.initStyle(StageStyle.UNDECORATED);    //brisanje _ [] X
+	            			//primaryStage.initStyle(StageStyle.UNIFIED);
+	            			//primaryStage.initStyle(StageStyle.UTILITY);
+	            			
+	            			stage.show();
+	            			
+	            			
+	            			//Modality.NONE, Modality.WINDOW_MODAL, Modality.APPLICATION_MODAL
+	            			//stage.initModality(Modality.APPLICATION_MODAL);
+	            			//stage.showAndWait();
+	            		} catch(Exception e) {
+	            			e.printStackTrace();
+	            //Util.LOGGER.log(Level.SEVERE, e.toString(), e);
+	            		}
+	                }
+	                else 
+	                   ispis = JOptionPane.showInputDialog("Pogrsno korisnicko ime ili lozinka! Pokusajte ponovo");
+	                    
+	            }
+	        }catch(SQLException e) { e.printStackTrace(); }
+	        finally {
+	             if(r != null)
+	                try { r.close(); } catch (SQLException e) {}
+	             if( s != null)
+	                 try { s.close();} catch (SQLException e) { e.printStackTrace(); }
+	             if(c != null)
+	                 try { c.close(); } catch (SQLException e) { e.printStackTrace(); }
+	         }
+	        
+		/*try {
 			//																//org.unibl.etf.prijava.
 			Parent root = (AnchorPane)FXMLLoader.load(getClass().getResource("SalterskiRadnikView.fxml"));
 			Scene scene = new Scene(root);
@@ -116,7 +177,7 @@ public class PrijavaController implements Initializable {
 		} catch(Exception e) {
 			e.printStackTrace();
 //Util.LOGGER.log(Level.SEVERE, e.toString(), e);
-		}
+		}*/
 		
 		
 		
