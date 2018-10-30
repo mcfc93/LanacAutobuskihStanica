f09856ec61cbf1d6b463ae76678b20a8824dc4f6
markoknezic <mknezic88@gmail.com>
2018-10-30 22:50:21 +0100

    Dodana procedura za prikaz svih autobuskih stanica

--- Procedure28.10.2018.sql
+++ Procedure28.10.2018.sql
@@ -1,5 +1,5 @@
 use bus;
-
+################################# Procedura za autentikaciju ################################
 delimiter $$
 create procedure checkAuthentication(in pUserName varchar(35), in pPassword varchar(35))
 begin 
@@ -7,3 +7,13 @@ begin
 	
 end $$
 delimiter ;
+#############################################################################################
+
+######################### Procedura za prikaz svih autobuskih stanica #######################
+delimiter $$
+create procedure showBusStation()
+begin 
+	select Naziv,Adresa,BrojPoste,BrojTelefona,BrojPerona from autobuska_stanica;	
+end $$
+delimiter ;
+#############################################################################################
