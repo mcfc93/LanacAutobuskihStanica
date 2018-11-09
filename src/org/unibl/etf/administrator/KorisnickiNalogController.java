edece533626b686c5a098ebc342e09798ebca41f
mcfc93 <www.milanp@hotmail.com>
2018-11-09 15:42:54 +0100

    Dodana validacija za promjenu lozinke.

--- src/org/unibl/etf/administrator/KorisnickiNalogController.java
+++ src/org/unibl/etf/administrator/KorisnickiNalogController.java
@@ -1,24 +1,24 @@
 package org.unibl.etf.administrator;
 
 import java.net.URL;
-import java.sql.CallableStatement;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
 
 import org.unibl.etf.prijava.PrijavaController;
-import org.unibl.etf.util.Util;
 import org.unibl.etf.zaposleni.Zaposleni;
-
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.validation.NumberValidator;
+import com.jfoenix.validation.RequiredFieldValidator;
+import com.jfoenix.validation.base.ValidatorBase;
 
+import javafx.beans.binding.Bindings;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 
 public class KorisnickiNalogController implements Initializable {
 	
@@ -26,7 +26,7 @@ public class KorisnickiNalogController implements Initializable {
     private JFXPasswordField staraLozinkaTextField;
 
     @FXML
-    private JFXPasswordField novaLoxinka1TextField;
+    private JFXPasswordField novaLozinka1TextField;
 
     @FXML
     private JFXPasswordField novaLozinka2TextField;
@@ -67,11 +67,114 @@ public class KorisnickiNalogController implements Initializable {
    		emailTextField.setText(zaposleni.getEmail());
    		strucnaSpremaTextField.setText(zaposleni.getStrucnaSprema());
    		zaposleni=null;
+   		
+   		/*
+   		potvrdiButton.disableProperty().bind(Bindings.createBooleanBinding(
+	    		() -> staraLozinkaTextField.getText().trim().isEmpty()
+	    				|| novaLozinka1TextField.getText().trim().isEmpty()
+	    					|| novaLozinka2TextField.getText().trim().isEmpty()
+	    						, staraLozinkaTextField.textProperty()
+	    							, novaLozinka1TextField.textProperty()
+	    								, novaLozinka2TextField.textProperty()
+	    									));
+   		*/
+   		/*
+   		staraLozinkaTextField.focusedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue)->{
+	        if(staraLozinkaTextField.getText().equals("student")) {
+	        	staraLozinkaTextField.validate();
+	        }
+	    });
+		*/
+   		/*
+   		novaLozinka2TextField.textProperty().addListener((ObservableValue<? extends String> observable, String oldValue, String newValue)->{
+   			if(!novaLozinka1TextField.getText().trim().isEmpty()
+	        		&& !novaLozinka2TextField.getText().trim().isEmpty()
+	        			&& !novaLozinka1TextField.getText().trim().equals(novaLozinka2TextField.getText().trim())) {
+	        	System.out.println("UNOS");
+	        	novaLozinka2TextField.validate();
+   			} else {
+   				System.out.println("RESET");
+   				novaLozinka2TextField.resetValidation();
+   			}
+	    });
+		*/
+   		
+
+   		ValidatorBase samePassword = new ValidatorBase("Nije jednako") {
+			@Override
+			protected void eval() {
+				setIcon(new ImageView());
+			    //setMessage("Input Required");
+				if(!novaLozinka1TextField.getText().trim().isEmpty()
+		        		&& !novaLozinka2TextField.getText().trim().isEmpty()
+		        			&& !novaLozinka1TextField.getText().equals(novaLozinka2TextField.getText())) {
+		        	 hasErrors.set(true);
+		        } else {
+		        	 hasErrors.set(false);
+		        }
+			}
+		};
+		
+//		novaLozinka1TextField.textProperty().addListener((ObservableValue<? extends String> observable, String oldValue, String newValue)->{
+//	    	/*if(!novaLozinka2TextField.getText().trim().isEmpty()
+//	    			&& !novaLozinka1TextField.getText().trim().equals(novaLozinka2TextField.getText().trim())) {
+//	    	*/
+//				if(!newValue.isEmpty()) {
+//	    		novaLozinka2TextField.validate();
+//	        }/* else {
+//	        	novaLozinka2TextField.resetValidation();
+//	        }*/
+//	    });
+		/*
+		novaLozinka2TextField.textProperty().addListener((ObservableValue<? extends String> observable, String oldValue, String newValue)->{
+	    	if(!novaLozinka1TextField.getText().equals(newValue)) {
+	    		novaLozinka2TextField.validate();
+	        } else {
+	        	novaLozinka2TextField.resetValidation();
+	        }
+	    });
+	    */
+	    
+   		//staraLozinkaTextField.getValidators().add(requiredFieldValidator);
+   		staraLozinkaTextField.getValidators().add(requredFieldValidator(staraLozinkaTextField));
+   		novaLozinka1TextField.getValidators().add(requredFieldValidator(novaLozinka1TextField));
+   		novaLozinka2TextField.getValidators().addAll(requredFieldValidator(novaLozinka2TextField), samePassword);
 	}
 	
     @FXML
     void potvrdi(ActionEvent event) {
-
+    	//if(staraLozinkaTextField.getText().equals() && novaLozinka1TextField.getText().equals(novaLozinka2TextField.getText())) {
+    		
+    	//}
+    	
+    	if(staraLozinkaTextField.validate() && novaLozinka1TextField.validate() && novaLozinka2TextField.validate()) {
+    		System.out.println("DUGME");
+    	}
     }
 	
+    
+    public ValidatorBase requredFieldValidator(JFXPasswordField textField) {
+    	ValidatorBase requiredFieldValidator = new RequiredFieldValidator();
+	    requiredFieldValidator.setMessage("Obavezan unos");
+	    requiredFieldValidator.setIcon(new ImageView());
+	    
+	    
+	    textField.focusedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue)->{
+	        if(!newValue) {
+	        	textField.validate();
+	        }
+	    });
+	    
+	    
+	    textField.textProperty().addListener((ObservableValue<? extends String> observable, String oldValue, String newValue)->{
+	        //if(textField.getText().trim().isEmpty()) {
+	        	textField.validate();
+	        //} else {
+	        //	textField.resetValidation();
+	        //}
+	    });
+	    
+	    
+	    return requiredFieldValidator;
+    }
 }
